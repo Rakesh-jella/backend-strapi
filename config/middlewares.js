3a98c9cca4c6d75a307b6f9708a4b16d6b54d783
Rakesh-jella <rakeshjella035@gmail.com>
2024-10-15 10:51:44 +0530

    first commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://127.0.0.1:5501'], // Allowing base URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true, // Include credentials (if needed)
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
